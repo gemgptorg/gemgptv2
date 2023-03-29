9444d896f6c28144045b1cb64591543d7f62b533
gemgptorg <gary@gemgpt.org>
2023-03-29 21:40:09 +0300

    Update HomeTwo.jsx

--- src/pages/HomeTwo.jsx
+++ src/pages/HomeTwo.jsx
@@ -5,16 +5,10 @@ import About from '../features/about/home-v2';
 import dataAbout from '../assets/fake-data/data-about';
 import RoadMap from '../features/roadmap/home-v2';
 import dataRoadmap from '../assets/fake-data/data-roadmap';
-import Project from '../features/project/home-v2';
-import dataProject from '../assets/fake-data/dataProject';
+
 import Work from '../features/work/home-v2';
 import dataWork from '../assets/fake-data/data-work';
-import dataTeam from '../assets/fake-data/data-team';
-import Team from '../features/team/home-v2';
-import Partner from '../features/partner';
-import dataPartner from '../assets/fake-data/data-partner';
-import Blog from '../features/blog/home-v2';
-import dataBlog from '../assets/fake-data/data-blog';
+
 import FAQ from '../features/faq/home-v2';
 import dataFaq from '../assets/fake-data/data-faq';
 
